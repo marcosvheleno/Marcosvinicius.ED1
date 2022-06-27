ee805c6e3ea074cc9ec1d392d5eaf29b8d539edc
marcosvheleno <71414870+marcosvheleno@users.noreply.github.com>
2022-06-27 19:30:10 -0300

    Teste Upload Github

---
+++ github-lab03.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int maim{
+
+    struct individuo {
+        int codigo_genetico[10];
+        float aptidao_abs;
+        float aptidao_relativa;
+    };
+
+    struct populacao {
+        struct individuo *pop;
+        int tamanho;
+    };
+    typedef struct
+
+
+
+
+
+}
+
